d806e394a986ce9dc6847152f2e8547b513e58c8
Usama Sulaiman <sulaiman.usama@gmail.com>
2021-05-06 07:08:19 +0530

    Init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="homepage">This is a web page</div>
+    </div>
+  )
+}
